fbad1b5ff2fcf35ed18c890e25864ca7458e628c
Anjali Zille <anjalizille@gmail.com>
2020-12-31 17:09:51 +0530

    landing page added

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,9 +6,9 @@ import LandingPageImage from '../images/LandingPageImage.png';
 import BkMark from '../images/BkMark.png';
 import Vector from '../images/Vector.png';
 import overlay from '../images/overlay.png'
-import mobileimg from '../images/Rectangle-71.png'
-
-
+import mobileimg from '../images/Rectangle_71.png'
+import paperrimg from '../images/paperrimg.png';
+import quote from '../images/Quote-section.png';
 
 function  LandingPage() {
     return(
@@ -20,6 +20,7 @@ function  LandingPage() {
            <img className="vector-img" src={Vector} />
            <h1 className="landingpage-heading"> Clearminds</h1>
            <p className="landingpage-para">Lorem ipsum dolor sit amet, consectetur </p>
+           <button className="btn-landingpage">Get Start</button>
            </div>
            <div className="landingpage-middle">
              
@@ -31,13 +32,25 @@ function  LandingPage() {
                 <button className="landingpage-btn">Get Started</button>
 
                 <div className="landingpage-middle-second">
-                    <img src={overlay}/>
+                    <img  className="overlay" src={overlay}/>
                     <h1 className="landingpage-middle-second-heading">Lorem ipsum dolor sit amet</h1>
                     <p className="landing-middle-second-para">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae minima dolorum nesciunt tempora mollitia doloremque consequuntur excepturi voluptate, deserunt, ad impedit quis minus porro facere magni eveniet? Libero, repudiandae corrupti. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Beatae minima dolorum nesciunt tempora mollitia doloremque cevzzzzeniet? Libero, repudiandae corrupti.</p>
-                   <img className="landingpage-card" src={mobileimg}/>
+                  
+                  <img className="landingpage-card1" src={mobileimg}/> 
+                   <img  className="landingpage-card1" src={paperrimg}/>
+                   <p className="landingpage-card-heading">Clearmind App</p>
+                   <p className="landingpage-card1-heading"> Wealth Creation</p>
+                   
                 </div>
 
+                <div className="quotearea">
+                <p><q className="quoteclearmind">❝ Clearmind brings the best plans that will suite your pocket and help to scale the ladder of success in stock . ladder of success in stock m Clearmind bring ❞</q></p>
+              {/* <img src={quote}/> */}
+                </div>
+             <div className="rectangle-subsction">
+
+             </div>
             
                 </div>
 
